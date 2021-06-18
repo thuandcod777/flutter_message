52938e0eb4e4ec25ff6e1e29259999d61bb962f9
thuandcod777 <m.q.thuan777@gmail.com>
2021-06-18 15:43:03 +0700

    model user and service and test service

---
+++ chat/test/user_service_test.dart
@@ -0,0 +1,46 @@
+import 'package:chat/src/models/user.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:rethinkdb_dart/rethinkdb_dart.dart';
+
+import 'helper.dart';
+import 'user_service_impl.dart';
+
+void main() {
+  Rethinkdb r = Rethinkdb();
+  Connection connection;
+  UserService sut;
+
+  setUp(() async {
+    connection = await r.connect(host: "127.0.0.1", port: 28015);
+    await createDb(r, connection);
+    sut = UserService(r, connection);
+  });
+
+  tearDown(() async {
+    await cleanDb(r, connection);
+  });
+
+  test('create a new user document in database', () async {
+    final user = User(
+      username: 'test',
+      photoUrl: 'url',
+      active: true,
+      lastseen: DateTime.now(),
+    );
+    final userWithId = await sut.connect(user);
+    expect(userWithId.id, isNotEmpty);
+  });
+
+  test('get online users', () async {
+    final user = User(
+        username: 'test',
+        photoUrl: 'url',
+        active: true,
+        lastseen: DateTime.now());
+
+    await sut.connect(user);
+    final users = await sut.online();
+
+    expect(users.length, 0);
+  });
+}
